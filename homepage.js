f1f93345727e0ca09f6053ea1748f869116bc93b
Rabia <rabianaz935@gmail.com>
2023-01-07 19:27:51 +0500

    Homepage js

--- homepage.js
+++ homepage.js
@@ -1,17 +1,17 @@
-$(".heading").click(function () {
+$(".heading").click(function() {
     $(".card").css({ "visibility": "visible" });
 });
 $('.bttn').click(function () {
-        var newli = $('input[name=listarea]').val();
-        $('ol').append('<li>' + newli + '</li>');
-        $(".searcharea").val('');
-        $(".list_section").css({ "visibility": "visible" });
-    });
-$("input[name=listarea]").keyup(function (e) {
+    var newli = $('input[name=listarea]').val();
+    $('ol').append('<li>' + newli + '</li>');
+    $(".searcharea").val('');
+    $(".list_section").css({ "visibility": "visible" });
+});
+$("input[name=listarea]").keyup(function(e) {
     if (e.keyCode == 13) {
         $(".bttn").click();
-    }
+    } 
 });
-$(document).on('dblclick', 'li', function () {
+$(document).on('dblclick', 'li', function() {
     $(this).toggleClass('del').fadeOut('slow');
 });
